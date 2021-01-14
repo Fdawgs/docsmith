42f6f20f8206bcc0e7ec325ce51cb676b29465cb
Frazer Smith <frazerry@outlook.com>
2021-01-14 13:29:22 +0000

    chore: add config and template files

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+	env: {
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		"airbnb-base",
+		"plugin:promise/recommended",
+		"plugin:jest/recommended",
+		"plugin:jsdoc/recommended",
+		"plugin:security/recommended",
+		"prettier",
+	],
+	parserOptions: {
+		sourceType: "module",
+		ecmaFeatures: {
+			impliedStrict: true,
+		},
+	},
+	plugins: ["import", "jest", "jsdoc", "promise", "security"],
+	root: true,
+	rules: {
+		"import/no-extraneous-dependencies": "error",
+		"no-multiple-empty-lines": [
+			"error",
+			{
+				max: 1,
+			},
+		],
+		"prefer-destructuring": "off",
+		"promise/prefer-await-to-callbacks": "warn",
+		"promise/prefer-await-to-then": "warn",
+	},
+};
