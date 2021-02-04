573513de6e838d43c6f2a5949edb6a505c7899ca
Frazer Smith <frazerry@outlook.com>
2021-02-04 10:40:29 +0000

    feat(plugins): add `embedhtmlimages` decorator plugin

---
+++ src/plugins/embed-html-images/embed-html-images.test.js
@@ -0,0 +1,137 @@
+const { cloneDeep } = require('lodash');
+const fs = require("fs");
+const Fastify = require("fastify");
+const isHtml = require("is-html");
+const raw = require("raw-body");
+const plugin = require(".");
+const getConfig = require("../../config");
+
+describe("Embed-HTML-Images plugin", () => {
+	let config;
+	let server;
+
+	beforeAll(async () => {
+		config = await getConfig();
+	});
+
+	beforeEach(() => {
+		server = Fastify();
+
+        server.addContentTypeParser("text/html", async (req, payload) => {
+            const res = await raw(payload);
+    
+            return res;
+        });
+	});
+
+	afterEach(() => {
+		server.close();
+	});
+
+	test("Should embed images into HTML", async () => {
+		const altConfig = cloneDeep(config);
+		altConfig.poppler.tempDirectory = './test_resources/test_files/';
+		server.post("/", (req, res) => {
+			res.send(server.embedHtmlImages(
+				req.body,
+			));
+		});
+		server.register(plugin, altConfig);
+
+		const response = await server.inject({
+			method: "POST",
+			url: "/",
+			body: fs.readFileSync(
+                "./test_resources/test_files/tester_bullet_issues-html.html",
+                { encoding: "UTF-8" }
+			),
+			headers: {
+				'content-type': 'text/html'
+			}
+        });
+        
+		expect(/alt=""/gm.exec(response.payload)).toBeNull();
+		expect(typeof response.payload).toBe("string");
+		expect(isHtml(response.payload)).toBe(true);
+	});
+
+	test("Should embed images into HTML and remove alt attribute from img tags", async () => {
+		const altConfig = cloneDeep(config);
+		altConfig.poppler.tempDirectory = './test_resources/test_files/';
+		server.post("/", (req, res) => {
+			res.send(server.embedHtmlImages(
+				req.body,
+				true
+			));
+		});
+		server.register(plugin, altConfig);
+
+		const response = await server.inject({
+			method: "POST",
+			url: "/",
+			body: fs.readFileSync(
+                "./test_resources/test_files/tester_bullet_issues-html.html",
+                { encoding: "UTF-8" }
+			),
+			headers: {
+				'content-type': 'text/html'
+			}
+        });
+        
+		expect(/alt=""/gm.exec(response.payload)).not.toBeNull();
+		expect(typeof response.payload).toBe("string");
+		expect(isHtml(response.payload)).toBe(true);
+	});
+
+	test("Should embed images into HTML and add trailing slash if missing from directory", async () => {
+		const altConfig = cloneDeep(config);
+		altConfig.poppler.tempDirectory = './test_resources/test_files';
+		server.post("/", (req, res) => {
+			res.send(server.embedHtmlImages(
+				req.body
+			));
+		});
+		server.register(plugin, altConfig);
+
+		const response = await server.inject({
+			method: "POST",
+			url: "/",
+			body: fs.readFileSync(
+                "./test_resources/test_files/tester_bullet_issues-html.html",
+                { encoding: "UTF-8" }
+			),
+			headers: {
+				'content-type': 'text/html'
+			}
+        });
+
+		expect(typeof response.payload).toBe("string");
+		expect(isHtml(response.payload)).toBe(true);
+	});
+
+	test("Should continue if it cannot find images to embed in specified directory", async () => {
+		server.post("/", (req, res) => {
+			res.send(server.embedHtmlImages(
+				req.body,
+				true
+			));
+		});
+		server.register(plugin, config);
+
+		const response = await server.inject({
+			method: "POST",
+			url: "/",
+			body: fs.readFileSync(
+                "./test_resources/test_files/tester_bullet_issues-html.html",
+                { encoding: "UTF-8" }
+			),
+			headers: {
+				'content-type': 'text/html'
+			}
+        });
+
+		expect(typeof response.payload).toBe("string");
+		expect(isHtml(response.payload)).toBe(true);
+	});
+
+});
---
+++ src/plugins/embed-html-images/index.js
@@ -0,0 +1,50 @@
+const fp = require("fastify-plugin");
+const fs = require("fs");
+const { JSDOM } = require("jsdom");
+const path = require("path");
+
+/**
+ * @author Frazer Smith
+ * @description Decorator plugin that adds function to embed images into HTML,
+ * after encoding with Base64.
+ * @param {Function} server - Fastify instance.
+ * @param {object} options - Fastify config values.
+ * @param {string} options.poppler.tempDirectory - directory for temporarily storing
+ * files during conversion.
+ */
+async function plugin(server, options) {
+	/**
+	 * @param {string} html - Valid HTML.
+	 * @param {boolean=} removeAlt - Removes alt attribute from img tags if set to `true`.
+	 * Useful for sending to clinical systems where img tags are stripped from received documents
+	 * (i.e. TPP's SystmOne).
+	 * @returns {string} - HTML with images embedded.
+	 */
+	function embedHtmlImages(html, removeAlt) {
+		const dom = new JSDOM(html);
+		const images = dom.window.document.querySelectorAll("img");
+		let directory = options.poppler.tempDirectory;
+		directory += directory.endsWith("/") ? "" : "/";
+
+		images.forEach((element) => {
+			if (fs.existsSync(directory + element.src)) {
+				const imgForm = path.extname(element.src).substring(1);
+				const imageAsBase64 = `data:image/${imgForm};base64,${fs.readFileSync(
+					directory + element.src,
+					"base64"
+				)}`;
+				element.setAttribute("src", imageAsBase64);
+
+				if (removeAlt === true) {
+					element.setAttribute("alt", "");
+				}
+			}
+		});
+
+		return dom.window.document.documentElement.outerHTML;
+	}
+
+	server.decorate("embedHtmlImages", embedHtmlImages);
+}
+
+module.exports = fp(plugin);
