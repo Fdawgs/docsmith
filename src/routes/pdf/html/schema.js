d4954df981dc12673e9fd049b4ed29ee6bf8953a
Frazer Smith <frazer.dev@outlook.com>
2021-08-27 11:20:23 +0100

    fix: add 400 and 415 responses to shared schema

--- src/routes/pdf/html/schema.js
+++ src/routes/pdf/html/schema.js
@@ -114,11 +114,17 @@ const pdfToHtmlPostSchema = {
 		),
 	response: {
 		200: S.string(),
+		400: S.ref("responses#/definitions/badRequest").description(
+			"Bad Request"
+		),
 		401: S.ref("responses#/definitions/unauthorized").description(
 			"Unauthorized"
 		),
 		406: S.ref("responses#/definitions/notAcceptable").description(
 			"Not Acceptable"
+		),
+		415: S.ref("responses#/definitions/unsupportedMediaType").description(
+			"Unsupported Media Type"
 		),
 		429: S.ref("responses#/definitions/tooManyRequests").description(
 			"Too Many Requests"
