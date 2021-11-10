6351a0ef76802b44085597bcb075c50dbfdfc1a5
Frazer Smith <frazer.dev@outlook.com>
2021-11-10 14:45:29 +0000

    refactor(plugins/rtf-to-html): provide generic error response

--- src/plugins/rtf-to-html/index.js
+++ src/plugins/rtf-to-html/index.js
@@ -95,7 +95,7 @@ async function plugin(server, options) {
 			res.header("content-type", `text/html`);
 		} catch (err) {
 			server.log.error(err);
-			res.badRequest(err);
+			throw res.badRequest();
 		}
 	});
 }
