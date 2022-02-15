6dce4e6d40153a9b39a3582666a08245335e6107
Frazer Smith <frazer.dev@outlook.com>
2022-02-15 16:58:55 +0000

    chore(routes): update cors inline comment

--- src/routes/docx/txt/index.js
+++ src/routes/docx/txt/index.js
@@ -54,7 +54,7 @@ async function route(server, options) {
 
 	// Register plugins
 	server
-		// Use CORS: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+		// Enable CORS if options passed
 		.register(cors, {
 			...options.cors,
 			methods: ["POST"],
