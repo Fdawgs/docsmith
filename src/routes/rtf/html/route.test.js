82d5f866c2016c850a28525aa101526cb2f88c71
Frazer Smith <frazer.dev@outlook.com>
2022-06-14 13:17:26 +0100

    test(routes): remove `await` in `Promise.all()`s

--- src/routes/rtf/html/route.test.js
+++ src/routes/rtf/html/route.test.js
@@ -44,100 +44,124 @@ describe("RTF-to-HTML route", () => {
 	});
 
 	test("Should return RTF file converted to HTML", async () => {
-		queryStrings.forEach(async (query) => {
-			const response = await server.inject({
-				method: "POST",
-				url: "/",
-				body: await fs.readFile(
-					"./test_resources/test_files/valid_rtf.rtf"
-				),
-				query,
-				headers: {
-					accept: "application/json, text/html",
-					"content-type": "application/rtf",
-				},
-			});
+		await Promise.all(
+			queryStrings.map(async (query) =>
+				server
+					.inject({
+						method: "POST",
+						url: "/",
+						body: await fs.readFile(
+							"./test_resources/test_files/valid_rtf.rtf"
+						),
+						query,
+						headers: {
+							accept: "application/json, text/html",
+							"content-type": "application/rtf",
+						},
+					})
+					.then((response) => {
+						expect(response.payload).toEqual(
+							expect.stringContaining(
+								"Ask not what your country can do for you"
+							)
+						);
+						expect(isHtml(response.payload)).toBe(true);
+						expect(response.headers).toMatchObject({
+							"content-type": "text/html",
+						});
+						expect(response.statusCode).toBe(200);
 
-			expect(response.payload).toEqual(
-				expect.stringContaining(
-					"Ask not what your country can do for you"
-				)
-			);
-			expect(isHtml(response.payload)).toBe(true);
-			expect(response.headers).toMatchObject({
-				"content-type": "text/html",
-			});
-			expect(response.statusCode).toBe(200);
-		});
+						return response.statusCode;
+					})
+			)
+		);
 	});
 
 	test("Should return HTTP status code 415 if file is missing", async () => {
-		queryStrings.forEach(async (query) => {
-			const response = await server.inject({
-				method: "POST",
-				url: "/",
-				query,
-				headers: {
-					accept: "application/json, text/html",
-					"content-type": "application/rtf",
-				},
-			});
+		await Promise.all(
+			queryStrings.map(async (query) =>
+				server
+					.inject({
+						method: "POST",
+						url: "/",
+						query,
+						headers: {
+							accept: "application/json, text/html",
+							"content-type": "application/rtf",
+						},
+					})
+					.then((response) => {
+						expect(JSON.parse(response.payload)).toEqual({
+							error: "Unsupported Media Type",
+							message: "Unsupported Media Type",
+							statusCode: 415,
+						});
+						expect(response.statusCode).toBe(415);
 
-			expect(JSON.parse(response.payload)).toEqual({
-				error: "Unsupported Media Type",
-				message: "Unsupported Media Type",
-				statusCode: 415,
-			});
-			expect(response.statusCode).toBe(415);
-		});
+						return response.statusCode;
+					})
+			)
+		);
 	});
 
 	test("Should return HTTP status code 415 if file with '.rtf' extension is not a valid RTF file", async () => {
-		queryStrings.forEach(async (query) => {
-			const response = await server.inject({
-				method: "POST",
-				url: "/",
-				body: await fs.readFile(
-					"./test_resources/test_files/invalid_rtf.rtf"
-				),
-				query,
-				headers: {
-					accept: "application/json, text/html",
-					"content-type": "application/rtf",
-				},
-			});
+		await Promise.all(
+			queryStrings.map(async (query) =>
+				server
+					.inject({
+						method: "POST",
+						url: "/",
+						body: await fs.readFile(
+							"./test_resources/test_files/invalid_rtf.rtf"
+						),
+						query,
+						headers: {
+							accept: "application/json, text/html",
+							"content-type": "application/rtf",
+						},
+					})
+					.then((response) => {
+						expect(JSON.parse(response.payload)).toEqual({
+							error: "Unsupported Media Type",
+							message: "Unsupported Media Type",
+							statusCode: 415,
+						});
+						expect(response.statusCode).toBe(415);
 
-			expect(JSON.parse(response.payload)).toEqual({
-				error: "Unsupported Media Type",
-				message: "Unsupported Media Type",
-				statusCode: 415,
-			});
-			expect(response.statusCode).toBe(415);
-		});
+						return response.statusCode;
+					})
+			)
+		);
 	});
 
 	test("Should return HTTP status code 415 if file media type is not supported by route", async () => {
-		queryStrings.forEach(async (query) => {
-			const response = await server.inject({
-				method: "POST",
-				url: "/",
-				body: await fs.readFile(
-					"./test_resources/test_files/valid_empty_html.html"
-				),
-				query,
-				headers: {
-					accept: "application/json, text/html",
-					"content-type": "application/html",
-				},
-			});
+		await Promise.all(
+			queryStrings.map(async (query) =>
+				server
+					.inject({
+						method: "POST",
+						url: "/",
+						body: await fs.readFile(
+							"./test_resources/test_files/valid_empty_html.html"
+						),
+						query,
+						headers: {
+							accept: "application/json, text/html",
+							"content-type": "application/html",
+						},
+					})
+					.then((response) => {
+						expect(JSON.parse(response.payload)).toEqual({
+							error: "Unsupported Media Type",
+							message: "Unsupported Media Type: application/html",
+							statusCode: 415,
+						});
+						expect(response.statusCode).toBe(415);
 
-			expect(JSON.parse(response.payload)).toEqual({
-				error: "Unsupported Media Type",
-				message: "Unsupported Media Type: application/html",
-				statusCode: 415,
-			});
-			expect(response.statusCode).toBe(415);
-		});
+						return response.statusCode;
+					})
+			)
+		);
 	});
 
 	test("Should return HTTP status code 406 if media type in `Accept` request header is unsupported", async () => {
