ec5b850ba1aaa22a34a68febe37a1ef204c60093
Frazer Smith <frazer.dev@outlook.com>
2022-11-01 12:40:48 +0000

    fix(routes): add `HEAD` to cors allowed methods (#1105)

--- src/routes/admin/healthcheck/index.js
+++ src/routes/admin/healthcheck/index.js
@@ -20,7 +20,7 @@ async function route(server, options) {
 		// Enable CORS if options passed
 		.register(cors, {
 			...options.cors,
-			methods: ["GET"],
+			methods: ["GET", "HEAD"],
 		});
 
 	server.route({
