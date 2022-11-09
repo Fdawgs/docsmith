99d2b82b4ff55265cb9202b4786d87912c7b04f9
Frazer Smith <frazer.dev@outlook.com>
2022-11-09 10:45:30 +0000

    test(server): add cors tests

--- src/server.test.js
+++ src/server.test.js
@@ -57,6 +57,11 @@ const expResHeadersJson = {
 	"content-type": expect.stringContaining("application/json"),
 };
 
+const expResHeadersText = {
+	...expResHeaders,
+	"content-type": expect.stringContaining("text/plain"),
+};
+
 const expResHeaders4xxErrors = {
 	...expResHeadersJson,
 	"keep-alive": undefined,
@@ -547,6 +552,268 @@ describe("Server Deployment", () => {
 				});
 				expect(response.headers).toEqual(expResHeadersJson);
 				expect(response.statusCode).toBe(406);
+			});
+		});
+	});
+
+	describe("CORS", () => {
+		let config;
+		let server;
+		let currentEnv;
+
+		beforeAll(() => {
+			Object.assign(process.env, {
+				CORS_ALLOWED_HEADERS:
+					"Accept, Accept-Encoding, Accept-Language, Authorization, Content-Type, Origin, X-Forwarded-For, X-Requested-With",
+				CORS_MAX_AGE: 7200,
+			});
+			currentEnv = { ...process.env };
+		});
+
+		afterEach(async () => {
+			// Reset the process.env to default after each test
+			Object.assign(process.env, currentEnv);
+
+			await server.close();
+		});
+
+		const corsTests = [
+			{
+				testName: "CORS Disabled",
+				envVariables: {
+					CORS_ORIGIN: "",
+				},
+				request: {
+					headers: {
+						origin: null,
+					},
+				},
+				expected: {
+					response: {
+						headers: {
+							basic: expResHeaders,
+							json: expResHeadersJson,
+							text: expResHeadersText,
+						},
+					},
+				},
+			},
+			{
+				testName: "CORS Enabled",
+				envVariables: {
+					CORS_ORIGIN: true,
+				},
+				request: {
+					headers: {
+						origin: "https://notreal.ydh.nhs.uk",
+					},
+				},
+				expected: {
+					response: {
+						headers: {
+							json: {
+								...expResHeadersJson,
+								"access-control-allow-origin":
+									"https://notreal.ydh.nhs.uk",
+							},
+							text: {
+								...expResHeadersText,
+								"access-control-allow-origin":
+									"https://notreal.ydh.nhs.uk",
+							},
+						},
+					},
+				},
+			},
+			{
+				testName: "Cors Enabled and Set to String",
+				envVariables: {
+					CORS_ORIGIN: "https://notreal.ydh.nhs.uk",
+				},
+				request: {
+					headers: {
+						origin: "https://notreal.ydh.nhs.uk",
+					},
+				},
+				expected: {
+					response: {
+						headers: {
+							json: {
+								...expResHeadersJson,
+								"access-control-allow-origin":
+									"https://notreal.ydh.nhs.uk",
+							},
+							text: {
+								...expResHeadersText,
+								"access-control-allow-origin":
+									"https://notreal.ydh.nhs.uk",
+							},
+						},
+					},
+				},
+			},
+			{
+				testName: "Cors Enabled and Set to Array of Strings",
+				envVariables: {
+					CORS_ORIGIN: [
+						"https://notreal.ydh.nhs.uk",
+						"https://notreal.sft.nhs.uk",
+					],
+				},
+				request: {
+					headers: {
+						origin: "https://notreal.ydh.nhs.uk",
+					},
+				},
+				expected: {
+					response: {
+						headers: {
+							json: {
+								...expResHeadersJson,
+								"access-control-allow-origin":
+									"https://notreal.ydh.nhs.uk",
+							},
+							text: {
+								...expResHeadersText,
+								"access-control-allow-origin":
+									"https://notreal.ydh.nhs.uk",
+							},
+						},
+					},
+				},
+			},
+			{
+				testName: "Cors Enabled and Set to Wildcard",
+				envVariables: {
+					CORS_ORIGIN: "*",
+				},
+				request: {
+					headers: {
+						origin: "https://notreal.ydh.nhs.uk",
+					},
+				},
+				expected: {
+					response: {
+						headers: {
+							json: {
+								...expResHeadersJson,
+								"access-control-allow-origin": "*",
+							},
+							text: {
+								...expResHeadersText,
+								"access-control-allow-origin": "*",
+							},
+						},
+					},
+				},
+			},
+		];
+		corsTests.forEach((testObject) => {
+			describe(`${testObject.testName}`, () => {
+				beforeAll(async () => {
+					Object.assign(process.env, testObject.envVariables);
+					config = await getConfig();
+				});
+
+				beforeEach(async () => {
+					server = Fastify();
+					await server.register(startServer, config).ready();
+				});
+
+				describe("/admin/healthcheck Route", () => {
+					test("Should return `ok`", async () => {
+						const response = await server.inject({
+							method: "GET",
+							url: "/admin/healthcheck",
+							headers: {
+								accept: "text/plain",
+								origin: testObject.request.headers.origin,
+							},
+						});
+
+						expect(response.payload).toBe("ok");
+						expect(response.headers).toEqual(
+							testObject.expected.response.headers.text
+						);
+						expect(response.statusCode).toBe(200);
+					});
+
+					// Only applicable if CORS enabled
+					if (testObject.envVariables.CORS_ORIGIN) {
+						test("Should return response to CORS preflight request", async () => {
+							const response = await server.inject({
+								method: "OPTIONS",
+								url: "/admin/healthcheck",
+								headers: {
+									"access-control-request-method": "GET",
+									origin: testObject.request.headers.origin,
+								},
+							});
+
+							expect(response.payload).toBe("");
+							expect(response.headers).toEqual({
+								...expResHeaders,
+								"access-control-allow-headers":
+									process.env.CORS_ALLOWED_HEADERS,
+								"access-control-allow-methods": "GET, HEAD",
+								"access-control-allow-origin":
+									testObject.envVariables.CORS_ORIGIN === "*"
+										? "*"
+										: testObject.request.headers.origin,
+								"access-control-max-age": String(
+									process.env.CORS_MAX_AGE
+								),
+								"content-type": undefined,
+								vary: "Origin",
+							});
+							expect(response.statusCode).toBe(204);
+						});
+					}
+
+					test("Should return HTTP status code 406 if media type in `Accept` request header is unsupported", async () => {
+						const response = await server.inject({
+							method: "GET",
+							url: "/admin/healthcheck",
+							headers: {
+								accept: "application/javascript",
+								origin: testObject.request.headers.origin,
+							},
+						});
+
+						expect(JSON.parse(response.payload)).toEqual({
+							error: "Not Acceptable",
+							message: "Not Acceptable",
+							statusCode: 406,
+						});
+						expect(response.headers).toEqual(
+							testObject.expected.response.headers.json
+						);
+						expect(response.statusCode).toBe(406);
+					});
+				});
+
+				describe("Undeclared Route", () => {
+					test("Should return HTTP status code 404 if route not found", async () => {
+						const response = await server.inject({
+							method: "GET",
+							url: "/invalid",
+							headers: {
+								accept: "application/json",
+								origin: testObject.request.headers.origin,
+							},
+						});
+
+						expect(JSON.parse(response.payload)).toEqual({
+							error: "Not Found",
+							message: "Route GET:/invalid not found",
+							statusCode: 404,
+						});
+						expect(response.headers).toEqual(
+							expResHeaders4xxErrors
+						);
+						expect(response.statusCode).toBe(404);
+					});
+				});
 			});
 		});
 	});
