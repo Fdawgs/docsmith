2244c198f6a486ff0356b3c355bf3cc8430cbcc0
Frazer Smith <frazer.dev@outlook.com>
2022-12-01 16:04:48 +0000

    perf(config): fixed set of object keys

--- src/config/config.test.js
+++ src/config/config.test.js
@@ -41,7 +41,7 @@ describe("Configuration", () => {
 		const PROC_LOAD_MAX_RSS_BYTES = "";
 		const PROC_LOAD_MAX_EVENT_LOOP_UTILIZATION = "";
 		const RATE_LIMIT_MAX_CONNECTIONS_PER_MIN = "";
-		const RATE_LIMIT_EXCLUDED_ARRAY = '["127.0.0.1"]';
+		const RATE_LIMIT_EXCLUDED_ARRAY = "";
 		const AUTH_BEARER_TOKEN_ARRAY = "";
 		const OCR_ENABLED = "";
 		const OCR_LANGUAGES = "";
@@ -111,8 +111,12 @@ describe("Configuration", () => {
 		expect(config.fastifyInit.http2).toBeUndefined();
 
 		expect(config.cors).toEqual({
-			origin: false,
+			allowedHeaders: null,
+			credentials: false,
+			exposedHeaders: null,
 			hideOptionsRoute: true,
+			maxAge: null,
+			origin: false,
 		});
 
 		expect(config.processLoad).toEqual({
@@ -123,7 +127,7 @@ describe("Configuration", () => {
 		});
 
 		expect(config.rateLimit).toEqual({
-			allowList: JSON.parse(RATE_LIMIT_EXCLUDED_ARRAY),
+			allowList: null,
 			continueExceeding: true,
 			hook: "onSend",
 			max: 1000,
@@ -434,7 +438,7 @@ describe("Configuration", () => {
 			expect(config.cors).toEqual({
 				origin: expected.origin,
 				allowedHeaders: CORS_ALLOWED_HEADERS,
-				credentials: expected?.credentials,
+				credentials: expected?.credentials || false,
 				exposedHeaders: CORS_EXPOSED_HEADERS,
 				hideOptionsRoute: true,
 				maxAge: CORS_MAX_AGE,
