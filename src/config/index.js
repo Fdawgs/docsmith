dc34603083b0c89091693c458d2e494b912796d7
Julian <39803323+NHS-juju@users.noreply.github.com>
2023-04-03 09:13:54 +0100

    fix(config): missing CORS_ORIGIN env variable throws error (#1385)

--- src/config/index.js
+++ src/config/index.js
@@ -19,13 +19,9 @@ const { description, license, version } = require("../../package.json");
  * @returns {boolean|Array<string>|string} CORS parameter.
  */
 function parseCorsParameter(param) {
-	if (param.toLowerCase().trim() === "true") {
-		return true;
-	}
-	if (param.toLowerCase().trim() === "false") {
-		return false;
-	}
-	if (param.includes(",")) {
+	if (param?.toLowerCase().trim() === "true") return true;
+	if (param?.toLowerCase().trim() === "false") return false;
+	if (param?.includes(",")) {
 		return param
 			.trim()
 			.split(",")
