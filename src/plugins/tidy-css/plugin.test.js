beb50f6ea9359e1e3a59a86104a1c9447f24fa2a
Frazer Smith <frazer.dev@outlook.com>
2023-04-04 09:24:49 +0100

    test(plugins/tidy-css): improve coverage (#1401)
    * test(plugins/tidy-css): use `toMatch()` over `.test().toBe()`
    
    * test(plugins/tidy-css): check css is tidied and minified
    
    * test(plugin/tidy-css): combines tests into `it.each()` loop

--- src/plugins/tidy-css/plugin.test.js
+++ src/plugins/tidy-css/plugin.test.js
@@ -1,6 +1,7 @@
 const fs = require("fs/promises");
 const Fastify = require("fastify");
 const isHtml = require("is-html");
+const { JSDOM } = require("jsdom");
 const plugin = require(".");
 const getConfig = require("../../config");
 
@@ -26,159 +27,101 @@ describe("Tidy-CSS plugin", () => {
 		await server.close();
 	});
 
-	it("Tidies CSS in HTML", async () => {
+	// TODO: use `it.concurrent.each()` once it is no longer experimental
+	it.each([
+		{
+			testName: "Tidies CSS in HTML",
+			file: "valid_bullet_issues_html.html",
+			expected: {
+				// Original font-family of test file
+				fonts: /font-family:Times/,
+			},
+		},
+		{
+			testName: "Tidies CSS in HTML and sets new font",
+			file: "valid_bullet_issues_html.html",
+			options: { fonts: "Arial" },
+			expected: {
+				fonts: /font-family:Arial/,
+			},
+		},
+		{
+			testName: "Tidies CSS in HTML and sets new font in quotation marks",
+			file: "valid_bullet_issues_html.html",
+			options: { fonts: 'Sans Serif, "Gill Sans"' },
+			expected: {
+				fonts: /Sans Serif","\\"Gill Sans\\"/,
+			},
+		},
+		{
+			testName: "Tidies CSS in HTML and sets new background color",
+			file: "valid_bullet_issues_html.html",
+			options: { backgroundColor: "white" },
+			expected: {
+				backgroundColor: /background-color:#fff/,
+				// Original font-family of test file
+				fonts: /font-family:Times/,
+			},
+		},
+		{
+			testName:
+				"Tidies CSS in HTML, sets new background color, and sets new font",
+			file: "valid_bullet_issues_html.html",
+			options: { backgroundColor: "white", fonts: "Arial" },
+			expected: {
+				backgroundColor: /background-color:#fff/,
+				fonts: /font-family:Arial/,
+			},
+		},
+		{
+			testName:
+				"Creates new style element if none exist in HTML but query string params passed",
+			file: "valid_empty_html.html",
+			options: { fonts: "Arial", backgroundColor: "white" },
+			expected: {
+				fonts: /font-family:Arial/,
+				backgroundColor: /background-color:#fff/,
+			},
+		},
+		{
+			testName:
+				"Continues to parse style elements with no type attribute in HTML",
+			file: "valid_no_style_type_html.html",
+		},
+	])("$testName", async ({ expected, file, options }) => {
 		server.post("/", (req, res) => {
-			res.send(server.tidyCss(req.body));
+			res.send(server.tidyCss(req.body, options));
 		});
 		await server.register(plugin).ready();
 
 		const response = await server.inject({
 			method: "POST",
 			url: "/",
-			body: await fs.readFile(
-				"./test_resources/test_files/valid_bullet_issues_html.html",
-				{ encoding: "UTF-8" }
-			),
+			// eslint-disable-next-line security/detect-non-literal-fs-filename
+			body: await fs.readFile(`./test_resources/test_files/${file}`, {
+				encoding: "UTF-8",
+			}),
 			headers: {
 				"content-type": "text/html",
 			},
 		});
 
-		expect(/;}|<!--|--!?>/.test(response.payload)).toBe(false);
+		const dom = new JSDOM(response.payload);
+		const style = dom.window.document.querySelector("style");
+
 		expect(isHtml(response.payload)).toBe(true);
+		// Check font-family is set to expected value
+		expect(style.innerHTML).toMatch(expected?.fonts || /./);
+		// Check background-color is set to expected value
+		expect(style.innerHTML).toMatch(expected?.backgroundColor || /./);
+		// Check CSS is combined into one style tag
+		expect(dom.window.document.querySelectorAll("style")).toHaveLength(1);
+		// Check CSS is tidied and minified
+		expect(style.innerHTML).not.toMatch(/;}|<!--|--!?>|\n|\r|\r\n/);
 		expect(response.statusCode).toBe(200);
 	});
 
-	it("Tidies CSS in HTML and set new font", async () => {
-		server.post("/", (req, res) => {
-			res.send(server.tidyCss(req.body, { fonts: "Arial" }));
-		});
-		await server.register(plugin).ready();
-
-		const response = await server.inject({
-			method: "POST",
-			url: "/",
-			body: await fs.readFile(
-				"./test_resources/test_files/valid_bullet_issues_html.html",
-				{ encoding: "UTF-8" }
-			),
-			headers: {
-				"content-type": "text/html",
-			},
-		});
-
-		expect(/font-family:Arial/.test(response.payload)).toBe(true);
-		expect(/;}|<!--|--!?>/.test(response.payload)).toBe(false);
-		expect(isHtml(response.payload)).toBe(true);
-		expect(response.statusCode).toBe(200);
-	});
-
-	it("Tidies CSS in HTML and set new font in quotation marks", async () => {
-		server.post("/", (req, res) => {
-			res.send(
-				server.tidyCss(req.body, { fonts: 'Sans Serif, "Gill Sans"' })
-			);
-		});
-		await server.register(plugin).ready();
-
-		const response = await server.inject({
-			method: "POST",
-			url: "/",
-			body: await fs.readFile(
-				"./test_resources/test_files/valid_bullet_issues_html.html",
-				{ encoding: "UTF-8" }
-			),
-			headers: {
-				"content-type": "text/html",
-			},
-		});
-
-		expect(
-			/font-family:"Sans Serif","\\"Gill Sans\\""/.test(response.payload)
-		).toBe(true);
-		expect(/;}|<!--|--!?>/.test(response.payload)).toBe(false);
-		expect(isHtml(response.payload)).toBe(true);
-		expect(response.statusCode).toBe(200);
-	});
-
-	it("Tidies CSS in HTML and set new background color", async () => {
-		server.post("/", (req, res) => {
-			res.send(server.tidyCss(req.body, { backgroundColor: "white" }));
-		});
-		await server.register(plugin).ready();
-
-		const response = await server.inject({
-			method: "POST",
-			url: "/",
-			body: await fs.readFile(
-				"./test_resources/test_files/valid_bullet_issues_html.html",
-				{ encoding: "UTF-8" }
-			),
-			headers: {
-				"content-type": "text/html",
-			},
-		});
-
-		expect(/background-color:#fff/.test(response.payload)).toBe(true);
-		expect(/;}|<!--|--!?>/.test(response.payload)).toBe(false);
-		expect(isHtml(response.payload)).toBe(true);
-		expect(response.statusCode).toBe(200);
-	});
-
-	it("Creates new style element if none exist but query string param passed", async () => {
-		server.post("/", (req, res) => {
-			res.send(
-				server.tidyCss(req.body, {
-					fonts: "Arial",
-					backgroundColor: "white",
-				})
-			);
-		});
-		await server.register(plugin, config).ready();
-
-		const response = await server.inject({
-			method: "POST",
-			url: "/",
-			body: await fs.readFile(
-				"./test_resources/test_files/valid_empty_html.html",
-				{ encoding: "UTF-8" }
-			),
-			headers: {
-				"content-type": "text/html",
-			},
-		});
-
-		expect(/font-family:Arial/.test(response.payload)).toBe(true);
-		expect(/background-color:#fff/.test(response.payload)).toBe(true);
-		expect(/;}|<!--|--!?>/.test(response.payload)).toBe(false);
-		expect(isHtml(response.payload)).toBe(true);
-		expect(response.statusCode).toBe(200);
-	});
-
-	it("Continues to parse style elements with no type attribute", async () => {
-		server.post("/", (req, res) => {
-			res.send(server.tidyCss(req.body));
-		});
-		await server.register(plugin, config).ready();
-
-		const response = await server.inject({
-			method: "POST",
-			url: "/",
-			body: await fs.readFile(
-				"./test_resources/test_files/valid_empty_style_html.html",
-				{ encoding: "UTF-8" }
-			),
-			headers: {
-				"content-type": "text/html",
-			},
-		});
-
-		expect(/;}|<!--|--!?>/.test(response.payload)).toBe(false);
-		expect(isHtml(response.payload)).toBe(true);
-		expect(response.statusCode).toBe(200);
-	});
-
-	it("Continues if it cannot find any CSS to tidy", async () => {
+	it("Continues if it cannot find any CSS in HTML to tidy", async () => {
 		server.post("/", (req, res) => {
 			res.send(server.tidyCss(req.body));
 		});
@@ -195,6 +138,7 @@ describe("Tidy-CSS plugin", () => {
 				"content-type": "text/html",
 			},
 		});
+
 		expect(isHtml(response.payload)).toBe(true);
 		expect(response.statusCode).toBe(200);
 	});
