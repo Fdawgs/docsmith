8a4373f051a79f633bff617dc26d98ad7ec976f8
Frazer Smith <frazer.dev@icloud.com>
2025-11-19 16:32:30 +0000

    chore: tidy dotfiles, remove redundant deps

--- scripts/esbuild.js
+++ scripts/esbuild.js
@@ -1,10 +1,9 @@
 "use strict";
 
 const esbuild = require("esbuild");
-const { globPlugin } = require("esbuild-plugin-glob");
 
 esbuild.build({
-	entryPoints: ["src/**/!(*.test).*"],
+	entryPoints: ["src/**"],
 	format: "cjs",
 	loader: {
 		".html": "copy",
@@ -17,6 +16,5 @@ esbuild.build({
 	outdir: "dist",
 	packages: "external",
 	platform: "node",
-	plugins: [globPlugin()],
 	target: "node20",
 });
